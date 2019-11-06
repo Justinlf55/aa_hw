4fafa8146c8459e390fc77d2e33ba1431dc29c61
your name hereJustinlf55 <Justinlfern@gmail.com>
2019-11-06 07:28:02 -0800

    W4D3

---
+++ W4D3/Simon/lib/simon.rb
@@ -0,0 +1,55 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    while !@game_over
+      take_turn
+      system('clear')
+    end
+
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+
+  end
+
+  def show_sequence
+    add_random_color
+    @seq.each do |color|
+      puts color
+      sleep 0.75
+      system("clear")
+      sleep 0.25
+    end
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+    seq << COLORS.sample
+  end
+
+  def round_success_message
+
+  end
+
+  def game_over_message
+
+  end
+
+  def reset_game
+
+  end
+end
